025bb95ed40db577bc966557117304d6b470bfb1
Andrew Crowley <44073467+andrewcrowley21@users.noreply.github.com>
2019-03-31 14:09:25 -0400

    Update main.c

--- src2/main.c
+++ src2/main.c
@@ -2,43 +2,39 @@
 #include <string.h>  
 #include <stdio.h> 
 #include <math.h>
-#include "quadSolver.h"
-#include "inputValidation.h" 
-#include "inputReader.h" 
+#include "qsSolve.h"
+#include "qsValidate.h" 
+#include "qsGetLine.h" 
 #include "answerChecker.h"
 
 
 
-//Main Method--------------------------------------------------------------------------------------
+
 int main (int argc, char *argv[]) { 
 
 //floats to store result of input validation
-float a=0;
-float b=0;
-float c=0;  
+float a = 0;
+float b = 0;
+float c = 0;  
 
 //doubles to pass to quadSolver
 double x1;
 double x2; 
-int cont = 1;
+int contin = 1;
 
 while(cont){
-	quadConstants * line = readFromConsole();
-
-	if(line != NULL){
-
-	a = validateInput(line->A);
-	b = validateInput(line->B);
-	c = validateInput(line->C);
-
-	}else{
-		cont = 0;
+	constants * line = readLine();
+	if (line != NULL) {
+		a = validateInput(line->A);
+		b = validateInput(line->B);
+		c = validateInput(line->C);
+	}
+	else{
+		contin = 0;
 	}
 
-	quadSolver(a,b,c,&x1,&x2);  
-
-
-	printWarnings(a,b,c,x1,x2);
+	quadSolver(a,b,c,&x1,&x2);
+	displayWarnings(a,b,c,x1,x2);
 
 }
 return 0;
