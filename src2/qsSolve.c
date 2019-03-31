e5142b156d8959c78c62c69af10c4ceb6c118ed9
Andrew Crowley <44073467+andrewcrowley21@users.noreply.github.com>
2019-03-31 14:14:07 -0400

    Update qsSolve.c

--- src2/qsSolve.c
+++ src2/qsSolve.c
@@ -1,5 +1,5 @@
 //quadsolver.c
-#include "quadSolver.h"
+#include "qsSolve.h"
 #include <math.h>
 #include "answerChecker.h"
 
@@ -8,23 +8,25 @@ stores calculations in x1 and x2,
 returns the number of real solutions to the quadratic
 */
 int quadSolver(double a, double b, double c, double * x1, double * x2){
-	if(isDeterminantZero(a,b,c)){
+	if (isDeterminantZero(a, b, c)) {
 		*x1 = (b * (-1))/(2 * a);
 		*x2 = 0.0/0.0;
 		return 1;
-	}else if(isDeterminantNegative(a,b,c)){
+	}
+	else if (isDeterminantNegative(a, b, c)) {
 		*x1 = 0.0/0.0;
 		*x2 = 0.0/0.0;
 		return 0;
-	}else{
+	}
+	else{
 		double determinant = sqrt((b * b)-4*a*c);
-    	double higher = (b * -1) + determinant;
-    	higher = higher / (2 * a);
-    	double lower = (b * -1) - determinant;
-    	lower = lower / (2 * a);
-    	*x1 = higher;
-    	*x2 = lower;
-    	return 2;
+    		double higher = (b * -1) + determinant;
+    		higher = higher / (2 * a);
+    		double lower = (b * -1) - determinant;
+    		lower = lower / (2 * a);
+    		*x1 = higher;
+    		*x2 = lower;
+    		return 2;
 	}
 }
 
@@ -43,7 +45,7 @@ int isDeterminantNegative(double a, double b, double c){
 int approxZero(double z){
 	return (z < 0.0000001 && z > -0.0000001)? 1 : 0;
 }
-int printWarnings(double a, double b, double c, double x1, double x2){
+int displayWarnings(double a, double b, double c, double x1, double x2){
 	if(x1 != x1){
 		printf("No Real Solutions Exist.\n");
 	}else{
